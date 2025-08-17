d42a106651da692eb62b7142a2fef4a7353caae9
Hunter Edward Wilde <hunter.wilde@connect.qut.edu.au>
2025-08-17 12:24:57 +1000

    Practical 2, second attempt

--- P02/cmdLine.c
+++ P02/cmdLine.c
@@ -5,7 +5,9 @@
 int main(int argc, char *argv[]) {
     int arrayOne[] = {12, 15, 2, 19, 43, 22};
     int length = arrayLength(arrayOne);
+    for (int i = 0; i < length; i++) {
+        printf("arrayOne[%d] = %d || ", i, arrayOne[i]);
+    }
     printf("The length of the array is %d\n", length);
-
     return 0;
 }
