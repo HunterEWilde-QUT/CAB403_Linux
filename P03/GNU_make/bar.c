ba285d97ee795504942ed8fa2559384950802c63
Hunter Edward Wilde <hunter.wilde@connect.qut.edu.au>
2025-08-17 15:39:55 +1000

    Practical 3, task 1 finished

---
+++ P03/GNU_make/bar.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "foo.h"
+
+int main() {
+    printHelloMake();
+    return 0;
+}
---
+++ P03/GNU_make/foo.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "foo.h"
+
+void printHelloMake(void) {
+    printf("Hello Practical 3 (part 2)\n");
+    return;
+}
