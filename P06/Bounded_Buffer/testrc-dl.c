b58e9c4f4a2586fd97b75e91934535fc35cbfec8
Hunter Edward Wilde <hunter.wilde@connect.qut.edu.au>
2025-08-25 11:15:02 +1000

    Practical 6, set up Task 1

---
+++ P06/Bounded_Buffer/testrc-dl.c
@@ -0,0 +1,24 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <string.h>    /* String handling */
+
+void *addMillion(void *arg);
+int globalVar =0;
+
+int main(int argc, char *argv[]) {
+	pthread_t thread1, thread2;
+    pthread_create(&thread1, NULL, addMillion, NULL);
+    pthread_create(&thread2, NULL, addMillion, NULL);
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);   
+    printf("\n\nTotal for globalVar = %d\n", globalVar);
+    return 0;
+}
+
+void *addMillion(void *ptr) {
+    for (int i = 0; i < 10000000; i++) {
+       globalVar++;
+    }     
+    
+    return NULL;
+}
