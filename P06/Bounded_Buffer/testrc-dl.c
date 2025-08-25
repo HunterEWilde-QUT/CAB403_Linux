a73479fb96c9642f9fcfadcb254a8168263d44a8
Hunter Edward Wilde <hunter.wilde@connect.qut.edu.au>
2025-08-25 11:29:59 +1000

    Practical 6, Task 1 completed

--- P06/Bounded_Buffer/testrc-dl.c
+++ P06/Bounded_Buffer/testrc-dl.c
@@ -2,23 +2,33 @@
 #include <stdio.h>
 #include <string.h>    /* String handling */
 
+int globalVar = 0;
+pthread_mutex_t mutex;
+
 void *addMillion(void *arg);
-int globalVar =0;
 
 int main(int argc, char *argv[]) {
-	pthread_t thread1, thread2;
-    pthread_create(&thread1, NULL, addMillion, NULL);
-    pthread_create(&thread2, NULL, addMillion, NULL);
-    pthread_join(thread1, NULL);
-    pthread_join(thread2, NULL);   
+    pthread_t thread1, thread2, thread3;
+    pthread_t threads[] = {thread1, thread2, thread3};
+    pthread_mutex_init(&mutex, NULL); // Initialise mutex lock
+    for(int i = 0; i < 3; i++) {
+        pthread_create(&threads[i], NULL, addMillion, NULL);
+    }
+    for(int i = 0; i < 3; i++) {
+        pthread_join(threads[i], NULL);
+    }
     printf("\n\nTotal for globalVar = %d\n", globalVar);
     return 0;
 }
 
 void *addMillion(void *ptr) {
+    if(pthread_mutex_lock(&mutex) != 0) {
+        perror("Failed to create mutex lock.");
+        return NULL;
+    }
     for (int i = 0; i < 10000000; i++) {
-       globalVar++;
-    }     
-    
+        globalVar++;
+    }
+    pthread_mutex_unlock(&mutex);
     return NULL;
 }
