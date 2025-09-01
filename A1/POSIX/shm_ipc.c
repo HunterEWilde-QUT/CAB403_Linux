e2b378136497922f392e9a474d24c7938d36ee0d
Hunter Edward Wilde <hunter.wilde@connect.qut.edu.au>
2025-09-01 11:51:06 +1000

    A1, POSIX: added comment to explain assignment of `shm->name`.

--- A1/POSIX/shm_ipc.c
+++ A1/POSIX/shm_ipc.c
@@ -61,7 +61,7 @@ bool create_shared_object( shared_memory_t* shm, const char* share_name ) {
     shm_unlink(share_name);
 
     // Assign share name to shm->name.
-    shm->name = share_name;
+    shm->name = share_name; // const char* == const char*
 
     // Create the shared memory object, allowing read-write access by all users,
     // and saving the resulting file descriptor in shm->fd. If creation failed,
