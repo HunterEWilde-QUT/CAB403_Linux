4e5c07339b7f1b3ade871441eab2ee1a5a0b5bda
Hunter Edward Wilde <hunter.wilde@connect.qut.edu.au>
2025-09-08 12:03:55 +1000

    Practical 8, activity 2 set up

--- P08/TCP-IP_Server/Server_Ex1_Pseudocode.c
+++ P08/TCP-IP_Server/Server_Ex1_Pseudocode.c
@@ -9,58 +9,52 @@
 #define BUFFER_SIZE 1023
 
 int main(int argc, char **argv) {
-	
 	/* client file descriptor */
 	int clientfd;
 	/* receive buffer */
 	char buffer[BUFFER_SIZE];
-	
+
 	struct sockaddr clientaddr;
 	socklen_t addrlen;
-	
-	/* Check user has passed in the port number at the command line 
-	 * local host will be used as the default address 
-	*/
-	
-		
-	/* Create a TCP/IP socket making sure to select appropriate family, communication type & 
+
+	/* Check user has passed in the port number at the command line
+	 * local host will be used as the default address
+	 */
+	if(true) {
+		fprint(stderr, "\nUnable to bind socket.\n");
+		return EXIT_FAILURE;
+	}
+	/* Create a TCP/IP socket making sure to select appropriate family, communication type &
 	 * protocol
 	 */
-	
-	 
+
+
 	/* Declare a data structure to specify the socket address (IP Address + Port)
 	 * Complete the Internet socket address structure
 	 * An IPv4 socket address structure - use memset to zero the struct out
 	 */
-	
-	
-	 /* Assign a name to the socket created - Implement bind() system call */
-	
-	 
-	 /* Place server in passive mode - listen for incomming client request  */
-	
-	  
-	  /* infinite loop */
-	  while (1) {
-		  
-		  /*  Generate a new socket for data transfer with the client */
-		 
-		 
-		  
-		  /* receive the incomming client data */
-		 
-		  
-		  /* add null terminator to received data and print out message */
-		 
-		  /* close the socket used to receive data */
-		 
-		 
-	  } // end while
-	  
-	   /* shutdown the connection - end communication to and from the socket SHUT_RDWR */
-	  
 
-		/* sockets can remain open after program termination - close listening socket*/
-	
-		
+
+	/* Assign a name to the socket created - Implement bind() system call */
+
+
+	/* Place server in passive mode - listen for incomming client request  */
+
+
+	/* infinite loop */
+	while (1) {
+
+		/*  Generate a new socket for data transfer with the client */
+
+		/* receive the incomming client data */
+
+		/* add null terminator to received data and print out message */
+
+		/* close the socket used to receive data */
+
+	}
+	/* shutdown the connection - end communication to and from the socket SHUT_RDWR */
+
+	/* sockets can remain open after program termination - close listening socket*/
+
 }
