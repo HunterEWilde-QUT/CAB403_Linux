4a7fa0d3b90633d25f37369b637c0aebb687e08d
Hunter Wilde <hunter.wilde@connect.qut.edu.au>
2025-09-28 17:09:57 +1000

    Get cmd args for call & safety

--- A2/call.c
+++ A2/call.c
@@ -1,5 +1,14 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[]) {
+    /*Get required input arguments*/
+    if (argc != 3) {
+        fprintf(stderr, "Usage: ./call {source floor} {destination floor}");
+        return EXIT_FAILURE;
+    }
+    char floor_src = atoi(argv[1]);
+    char floor_dst = atoi(argv[2]);
+
     return EXIT_SUCCESS;
 }
--- A2/safety.c
+++ A2/safety.c
@@ -1,5 +1,12 @@
 #include "internal.h"
 
 int main(int argc, char *argv[]) {
+    /*Get required input arguments*/
+    if (argc != 2) {
+        fprintf(stderr, "Usage: ./safety {car name}");
+        return EXIT_FAILURE;
+    }
+    char car_name = atoi(argv[1]);
+
     return EXIT_SUCCESS;
 }
