576563cb119854fccf6e7c8c61d38e3af1a166ba
Hunter Edward Wilde <hunter.wilde@connect.qut.edu.au>
2025-09-29 17:10:38 +1000

    A2: update handling of char pointers in `call.c` (declare before defining; assign using `strcpy()`).

--- A2/call.c
+++ A2/call.c
@@ -1,14 +1,22 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
+    /**
+     * Declare variables
+     * Floor names are always 1 to 3 characters.
+     */
+    char floor_src[4];
+    char floor_dst[4];
+
     /*Get required input arguments*/
     if (argc != 3) {
         fprintf(stderr, "Usage: ./call {source floor} {destination floor}");
         return EXIT_FAILURE;
     }
-    char floor_src = atoi(argv[1]);
-    char floor_dst = atoi(argv[2]);
+    strcpy(floor_src, atoi(argv[1]));
+    strcpy(floor_dst, atoi(argv[2]));
 
     return EXIT_SUCCESS;
 }
