c5f2ad397cd47032dc3666c0d337d6fdc13ff43c
Hunter Wilde <hunter.wilde@connect.qut.edu.au>
2025-09-30 17:05:29 +1000

    A2: implement operation handling in `internal.c`.

--- A2/internal.c
+++ A2/internal.c
@@ -1,4 +1,5 @@
 #include "internal.h"
+#include "status.h"
 
 int main(int argc, char *argv[]) {
     /*Handle invalid usage*/
@@ -8,23 +9,121 @@ int main(int argc, char *argv[]) {
     }
 
     /*Declare variables*/
-    char *car_name;
-    char *operation;
     car_shmem_ctrl *car;
+    char *operation;
 
     /*Get required input arguments*/
-    strcpy(car_name, argv[1]);
+    strcpy(car->name, str_car); // Initialise name of car object as "car"
+    strcat(car->name, argv[1]); // Append car_name (e.g. "A") onto name of car object: "carA"
     strcpy(operation, argv[2]);
 
     /*Get shared memory structure from name*/
-    car->fd = shm_open(car_name, O_RDWR, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+    car->fd = shm_open(car->name, O_RDWR, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
     if (car->fd == -1) {
-        fprintf(stderr, "Unable to open shared memory segment.");
+        fprintf(stderr, "Unable to access car %s.", car->name);
         return EXIT_FAILURE;
     }
     car->data = mmap(NULL, sizeof(car_shared_mem), PROT_READ | PROT_WRITE, MAP_SHARED, car->fd, 0);
     if (car->data == MAP_FAILED) {
-        fprintf(stderr, "Unable to map shared memory segment.");
+        fprintf(stderr, "Unable to map shared memory segment for car %s.", car->name);
+        return EXIT_FAILURE;
+    }
+
+    /*Handle operations*/
+    if (operation == "open") {
+        car->data->open_button = 1;
+    } else if (operation == "close") {
+        car->data->close_button = 1;
+    } else if (operation == "stop") {
+        car->data->emergency_stop = 1;
+    } else if (operation == "service_on") {
+        car->data->individual_service_mode = 1;
+        car->data->emergency_mode = 0;
+    } else if (operation == "service_off") {
+        car->data->individual_service_mode = 0;
+    } else if (operation == "up" || operation == "down") {
+        /*Check prerequisites to manual floor traversal*/
+        if (car->data->individual_service_mode != 1) {
+            fprintf(stderr, "Operation only allowed in service mode.");
+            return EXIT_FAILURE;
+        }
+        if (car->data->status == str_open || car->data->status == str_opening || car->data->status == str_closing) {
+            fprintf(stderr, "Operation not allowed while doors are open.");
+            return EXIT_FAILURE;
+        }
+        if (car->data->status == str_between) {
+            fprintf(stderr, "Operation not allowed while elevator is moving.");
+            return EXIT_FAILURE;
+        }
+
+        uint8_t next_lvl;   // Next integer **level**.
+        uint8_t B;          // Presence of 'B' prefix denoting basement level.
+
+        /*Check for non-integer floor name (i.e. a basement floor)*/
+        if (car->data->current_floor[0] == 'B') {
+            /*B99 -> B1*/
+            B = 1;
+            next_lvl = atoi(&car->data->current_floor[1]); // Get numbers after 'B'.
+        } else {
+            /*1 -> 999*/
+            B = 0;
+            next_lvl = atoi(car->data->current_floor);
+        }
+
+        if (operation == "up") {
+            if (B) {
+                /*B99 -> B1*/
+                next_lvl--; // Rise by one floor.
+            } else {
+                /*1 -> 999*/
+                next_lvl++; // Rise by one floor.
+            }
+
+            /**
+             * Check if next floor is a possible position:
+             * If the next level is 0, the car has risen from floor B1 to floor 1.
+             * If the next level is 1000, the car has reached the highest floor of 999.
+             */
+            if (next_lvl == 0) {
+                strcpy(car->data->destination_floor, "1");
+            } else if (next_lvl > 999) {
+                fprintf(stderr, "Car cannot rise above highest floor.");
+                return EXIT_FAILURE;
+            }
+        } else if (operation == "down") {
+            if (B) {
+                /*B99 -> B1*/
+                next_lvl++; // Fall by one floor.
+            } else {
+                /*1 -> 999*/
+                next_lvl--; // Fall by one floor.
+            }
+
+            /**
+             * Check if next floor is a possible position:
+             * If the next level is 0, the car has fallen from floor 1 to floor B1.
+             * If the next leve is 100, the car has reached the lowest floor of B99.
+             */
+            if (next_lvl == 0) {
+                strcpy(car->data->destination_floor, "B1");
+            } else if (next_lvl > 99) {
+                fprintf(stderr, "Car cannot fall below lowest floor.");
+                return EXIT_FAILURE;
+            }
+        }
+
+        /*Write to the car's destination floor*/
+        char next_floor[4];
+        sprintf(next_floor, "%u", next_lvl); // Convert unsigned int to string.
+
+        if (B) {
+            strcpy(car->data->destination_floor, "B");
+            strcat(car->data->destination_floor, next_floor);
+        } else {
+            strcpy(car->data->destination_floor, next_floor);
+        }
+    } else {
+        fprintf(stderr, "Invalid operation.");
         return EXIT_FAILURE;
     }
 
