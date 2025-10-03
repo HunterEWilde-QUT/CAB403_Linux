b2365ffcc7258fe177c64d4c13eebdafbcc16022
Hunter Wilde <hunter.wilde@connect.qut.edu.au>
2025-10-03 14:56:07 +1000

    A2: implement safety system.

--- A2/safety.c
+++ A2/safety.c
@@ -1,5 +1,12 @@
+#include <regex.h>
+
 #include "internal.h"
 #include "status.h"
+
+int check_valid_floors(car_shared_mem *);
+int check_valid_status(char *);
+int check_valid_states(car_shared_mem *);
+int check_door_obstruction(car_shared_mem *);
 
 int main(int argc, char *argv[]) {
     /*Handle invalid usage*/
@@ -27,5 +34,64 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
+    /*Handle conditions*/
+    if (car->data->safety_system != 1) {
+        car->data->safety_system = 1;
+    }
+    if (car->data->door_obstruction && car->data->status == str_closing) {
+        /*Obstruction detected*/
+        strcpy(car->data->status, str_closing);
+    }
+    if (car->data->emergency_stop && !car->data->emergency_mode) {
+        fprintf(stderr, "The emergency stop button has been pressed!");
+        car->data->emergency_mode = 1;
+        car->data->emergency_stop = 0;
+    }
+    if (car->data->overload && !car->data->emergency_mode) {
+        fprintf(stderr, "The overload sensor has been tripped!");
+        car->data->emergency_mode = 1;
+    }
+    if (!car->data->emergency_mode && (check_valid_floors(car->data)
+        || check_valid_status(car->data->status)
+        || check_valid_states(car->data)
+        || check_door_obstruction(car->data)))
+    {
+        fprintf(stderr, "Data consistency error!");
+        car->data->emergency_mode = 1;
+    }
+
     return EXIT_SUCCESS;
+}
+
+int check_valid_floors(car_shared_mem *data) {
+    regex_t valid_floor; // Declare a regular expression.
+    regcomp(&valid_floor, "^B?[1-9][0-9]*$", 0); // Compile regex for testing floor name validity.
+    int valid = regexec(&valid_floor, car->data->current_floor, 0, NULL, 0) != 0
+        || regexec(&valid_floor, car->data->destination_floor, 0, NULL, 0) != 0;
+    regfree(&valid_floor); // Not needed hereafter.
+
+    return valid;
+}
+
+int check_valid_status(char status[8]) {
+    return status != str_open
+        && status != str_opening
+        && status != str_closed
+        && status != str_closing
+        && status != str_between;
+}
+
+int check_valid_states(car_shared_mem *data) {
+    return data->open_button > 1
+        || data->close_button > 1
+        || data->safety_system > 1
+        || data->door_obstruction > 1
+        || data->overload > 1
+        || data->emergency_stop > 1
+        || data->individual_service_mode > 1;
+}
+
+int check_door_obstruction(car_shared_mem *data) {
+    return data->door_obstruction && (data->status != str_opening
+        || data->status != str_closing);
 }
