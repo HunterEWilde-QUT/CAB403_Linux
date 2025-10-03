1bda31bc68ac9702a0c0b3f34603d2547b0e02a5
Hunter Wilde <hunter.wilde@connect.qut.edu.au>
2025-10-03 14:19:02 +1000

    A2: get shared memory object for safety system.

--- A2/safety.c
+++ A2/safety.c
@@ -1,4 +1,5 @@
 #include "internal.h"
+#include "status.h"
 
 int main(int argc, char *argv[]) {
     /*Handle invalid usage*/
@@ -8,10 +9,23 @@ int main(int argc, char *argv[]) {
     }
 
     /*Declare variables*/
-    char *car_name;
+    car_shmem_ctrl *car;
 
-    /*Get required input arguments*/
-    strcpy(car_name, argv[1]);
+    /*Get name*/
+    strcpy(car->name, str_car); // Initialise name of car object as "car"
+    strcat(car->name, argv[1]); // Append car_name (e.g. "A") onto name of car object: "carA"
+
+    /*Get shared memory structure from name*/
+    car->fd = shm_open(car->name, O_RDWR, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+    if (car->fd == -1) {
+        fprintf(stderr, "Unable to access car %s.", car->name);
+        return EXIT_FAILURE;
+    }
+    car->data = mmap(NULL, sizeof(car_shared_mem), PROT_READ | PROT_WRITE, MAP_SHARED, car->fd, 0);
+    if (car->data == MAP_FAILED) {
+        fprintf(stderr, "Unable to map shared memory segment for car %s.", car->name);
+        return EXIT_FAILURE;
+    }
 
     return EXIT_SUCCESS;
 }
