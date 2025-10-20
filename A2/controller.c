298b698a22ab16c4cd788e08ef7eefcf6957c47d
Hunter Wilde <hunter.wilde@connect.qut.edu.au>
2025-10-20 20:28:45 +1000

    A2: add thread handling & linked list to `controller`; client request logic WIP

--- A2/controller.c
+++ A2/controller.c
@@ -1,4 +1,5 @@
 #include <pthread.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -8,15 +9,22 @@
 const int opt_enable = 1;
 const int BUFFERLEN = 1023;
 
-typedef struct
+/**
+ * For creating linked list of threads for incoming TCP requests.
+ * Facilitates tracking of all threads so that they may be joined neatly upon program completion.
+ */
+typedef struct tcp_thread_node_struct tcp_thread_node;
+struct tcp_thread_node_struct
 {
-	pthread_t thread;
-	tcp_thread_node* next;
-} tcp_thread_node;
+	pthread_t thread;		// Thread object to be created for handling client request (i.e. car or call pad)
+	uint8_t thread_created;	// Boolean to track if this thread has been created yet
+	tcp_thread_node* next;	// Pointer to next node in linked list
+};
 
-void* handle_connection(void*);	// Thread to handle incomming requests
-void connect_car(void);			// Car request logic
-void connect_call(void);		// Call pad request logic
+pthread_t link_thread_node(tcp_thread_node*);
+void* handle_connection(void*);
+void connect_car(const int*, char*);
+void send_car(char*);
 
 /**
  * Handles routing of cars requested by call pads.
@@ -30,10 +38,6 @@ int main(void)
 
     struct sockaddr_in server_addr;
     struct sockaddr client_addr;
-
-	/*Create a linked list for car threads*/
-	tcp_thread_node* head;
-	car_thread->next = NULL;
 
     /*Initialise listening socket*/
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -65,6 +69,11 @@ int main(void)
         fprintf(stderr, "\nUnable to listen for clients.\n");
     }
 
+	/*Initialise a linked list for car threads*/
+	tcp_thread_node* head;
+	head->thread_created = 0;
+	head->next = NULL;
+
 	while (1)
     {
         /*Accept an incoming client request*/
@@ -76,33 +85,48 @@ int main(void)
         }
 
 		/*Create thread to handle request*/
-		if (pthread_create(&node->thread, NULL, handle_connection, clientfd) != 0)
+		pthread_t current_thread = link_thread_node(head);
+		if (pthread_create(&current_thread, NULL, handle_connection, clientfd) != 0)
 		{
 			fprintf(stderr, "\nUnable to create controller thread.\n");
 			return EXIT_FAILURE;
 		}
+		pthread_detach(current_thread); // Allow thread to self-terminate when finished
 
 		//break;
     }
 
-	/*Terminate threads*/
-	//pthread_join(head->thread, NULL);
-	//if (head->next != NULL)
-	//{
-	//
-	//}
-
-    if (shutdown(clientfd, SHUT_RDWR) == -1)
-    {
-        fprintf(stderr, "\nUnable to shut down client.\n");
-        return EXIT_FAILURE;
-    }
-
-    close(sockfd);
-
     return EXIT_SUCCESS;
 }
 
+/**
+ * Adds a new TCP thread node to the linked list.
+ * @param node Node in the linked list.
+ * @return Thread object of the new node.
+ */
+pthread_t link_thread_node(tcp_thread_node* node)
+{
+	if (!node->thread_created) // This node hasn't been used yet (only true for head node)
+	{
+		node->thread_created = 1;
+		return node->thread;
+	}
+	if (node->next == NULL) // This is the tail node; a new node is needed
+	{
+		tcp_thread_node* new;	// Create a new node
+		node->next = new;		// Link this node to the new node
+		new->next = NULL;		// New node is the new tail
+		new->thread_created = 1;
+		return new->thread;
+	}
+	// This node is not the tail; must traverse linked list
+	return link_thread_node(node->next);
+}
+
+/**
+ * Reads the message in an incoming TCP request & conditionally calls the relevant function to handle it.
+ * @param ptr Client file descriptor: identifies the connection to a car or call pad.
+ */
 void* handle_connection(void* ptr)
 {
 	const int* clientfd = ptr;
@@ -118,16 +142,44 @@ void* handle_connection(void* ptr)
     buffer[bytesrcv] = '\0'; // Add null terminator
 
 	/*Direct to relevant handler*/
+	if (strstr(buffer, "CAR") != NULL) // A new car has been created
+	{
+		connect_car(clientfd, buffer);
+	}
+	else if (strstr(buffer, "CALL") != NULL) // A call pad is summoning a car
+	{
+		send_car(buffer);
+	}
 
+	return NULL;
 }
 
-void connect_car(void)
+/**
+ * Register a new car & handle 2-way communication.
+ */
+void connect_car(const int* clientfd, char* buffer)
 {
+	/*Add car to register*/
 
-	close(clientfd); // Close socket
+
+	/*Terminate connection in case of service mode or emergency*/
+	if (strstr(buffer, "INDIVIDUAL SERVICE") != NULL
+		|| strstr(buffer, "EMERGENCY") != NULL)
+	{
+		// Not sure if this is needed
+		// if (shutdown(clientfd, SHUT_RDWR) == -1)
+		// {
+		// 	fprintf(stderr, "\nUnable to shut down client.\n");
+		// 	return EXIT_FAILURE;
+		// }
+		close(*clientfd); // Close socket
+	}
 }
 
-void connect_call(void)
+/**
+ * Process a request from a call pad & add its source floor to a registered car's queue.
+ */
+void send_car(char* buffer)
 {
 
 }
